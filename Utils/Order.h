a3fd2ebebf20302a88ac82d7709045f00801bd7e
nikitosik_chv <chernovskiy.nikita@mail.ru>
2025-03-15 11:35:39 +0300

    Add files via upload

--- Utils/Order.h
+++ Utils/Order.h
@@ -1,8 +1,16 @@
-//
-// Created by chern on 15.03.2025.
-//
-
 #ifndef ORDER_H
 #define ORDER_H
+#include "Drink.h"
+
+class Order {
+    int orderID;
+    Drink drink;
+    std::string status;
+    public:
+        Order(int id, Drink drink, std::string status): orderID(id), drink(drink), status(status) {};
+        Drink& getDrink() {
+            return drink;
+        };
+};
 
 #endif //ORDER_H
